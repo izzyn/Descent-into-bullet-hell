c29a5fcdeff3e047b136614444d7e08f1733149d
Isak Nilsson <isak.nilsson7@learnet.se>
2022-09-14 15:27:10 +0200

    Added documentatio

--- Assets/bossAttack.cs
+++ Assets/bossAttack.cs
@@ -35,14 +35,14 @@ public class bossAttack: MonoBehaviour
         currentPhase = pickAttack(phaseAttacksList[0].attackBasicList);
         StartCoroutine(currentPhase); //makes the boss pick attacks when it loads in
     }
-    public void updatePhase(int hp, List<phaseAttacks> listPhaseAttacks)
+    public void updatePhase(int hp, List<phaseAttacks> listPhaseAttacks) //Updates phase every time the boss loses HP
     {
-        for(int i = 1; i < listPhaseAttacks.Count; i++)
+        for(int i = 1; i < listPhaseAttacks.Count; i++) 
         {
             if(listPhaseAttacks[i].hpThreshold == hp)
             {
                 StopCoroutine(currentPhase);
-                IEnumerator newPhase = pickAttack(phaseAttacksList[i].attackBasicList);
+                IEnumerator newPhase = pickAttack(phaseAttacksList[i].attackBasicList); //Sets the new attack list to the one that corresponds to that phase
                 StartCoroutine(newPhase);
                 currentPhase = newPhase;
                 break;
@@ -67,17 +67,17 @@ public class bossAttack: MonoBehaviour
     }
     IEnumerator spawnGuns(attackBasic attackInfo)
     {
-        for(int i = 0; i < attackInfo.spawnInfo.gunInfo.Count; i++)
+        for(int i = 0; i < attackInfo.spawnInfo.gunInfo.Count; i++) //Spawns a gun for each gun in the attacks's list
         {
-            GameObject gunSpawned = Instantiate(attackInfo.spawnInfo.gunInfo[i].gun);
-            Vector2 savedScale = gunSpawned.transform.localScale;
+            GameObject gunSpawned = Instantiate(attackInfo.spawnInfo.gunInfo[i].gun);i //Creates an object using the information provided
+            Vector2 savedScale = gunSpawned.transform.localScale; 
             attackInfo.spawnInfo.gunInfo[i].bulletConfig.lockedToGun = attackInfo.spawnInfo.gunInfo[i].rotationConfig.lockedToGun;
             gunSpawned.transform.localScale = new Vector2(savedScale.x * attackInfo.spawnInfo.gunInfo[i].gunScaleX, savedScale.y * attackInfo.spawnInfo.gunInfo[i].gunScaleY);
             gunSpawned.transform.position = new Vector3(attackInfo.spawnInfo.gunInfo[i].spawnLocation.transform.position.x, attackInfo.spawnInfo.gunInfo[i].spawnLocation.transform.position.y, -3);
             gunSpawned.GetComponent<SpriteRenderer>().sprite = attackInfo.spawnInfo.gunInfo[i].gunTexture;
             if(gunSpawned.GetComponent<SpriteRenderer>().sharedMaterial == attackInfo.spawnInfo.gunInfo[i].bulletConfig.bullet.GetComponent<SpriteRenderer>().sharedMaterial)
             {
-                changeColour(attackInfo.spawnInfo.gunInfo[i].bulletConfig.colourConfig.stage, attackInfo.spawnInfo.gunInfo[i].bulletConfig.colourConfig.Colour, gunSpawned);
+                changeColour(attackInfo.spawnInfo.gunInfo[i].bulletConfig.colourConfig.stage, attackInfo.spawnInfo.gunInfo[i].bulletConfig.colourConfig.Colour, gunSpawned); //Green screens the gun to the same colour as the bullet
             }
             Vector3 newScale = gameObject.transform.localScale;
             if (attackInfo.spawnInfo.gunInfo[i].shootTowardsPlayer != true)
@@ -85,7 +85,7 @@ public class bossAttack: MonoBehaviour
                 gunSpawned.transform.Rotate(new Vector3(0, 0, attackInfo.spawnInfo.gunInfo[i].rotation));
                 if ((gunSpawned.transform.rotation.eulerAngles.z * Mathf.Rad2Deg) > 90f || (gunSpawned.transform.rotation.eulerAngles.z) < -90f)
                 {
-                    Debug.Log("Yes");
+                    Debug.Log("Yes"); //Flips the gun sprite so it doesn't look weird when rotating
                     gunSpawned.GetComponent<SpriteRenderer>().flipY = true;
                 }
             }
@@ -93,93 +93,93 @@ public class bossAttack: MonoBehaviour
             {
                 GameObject player = GameObject.FindGameObjectWithTag("Player");
                 Vector3 target = player.transform.position - gunSpawned.transform.position;
-                float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
-                gunSpawned.transform.Rotate(new Vector3(0, 0, angle));
+                float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg; //Creates an angle from the gun to the player
+                gunSpawned.transform.Rotate(new Vector3(0, 0, angle)); //Rotates the gun to that angle
                 if (((gunSpawned.transform.rotation.eulerAngles.z) > 90) && ((gunSpawned.transform.rotation.eulerAngles.z) < 270))
                 {
-                    gunSpawned.GetComponent<SpriteRenderer>().flipY = true;
+                    gunSpawned.GetComponent<SpriteRenderer>().flipY = true; //Making sure the sprite looks good while rotating
                 }
                 else
                 {
                     gunSpawned.GetComponent<SpriteRenderer>().flipY = false;
                 }
             }
-            if(attackInfo.spawnInfo.gunInfo[i].tracksPlayer)
+            if(attackInfo.spawnInfo.gunInfo[i].tracksPlayer) //Makes the gun rotation follow the player if that setting is on
             {
                 StartCoroutine(followPlayer(attackInfo.spawnInfo.gunInfo[i], gunSpawned));
             }
-            if (attackInfo.spawnInfo.gunInfo[i].rotatesOverTime)
+            if (attackInfo.spawnInfo.gunInfo[i].rotatesOverTime) //Same as above but rotating over time instead of towards the player
             {
                 StartCoroutine(rotateGun(attackInfo.spawnInfo.gunInfo[i], gunSpawned));
             }
-            StartCoroutine(spawnBullets(attackInfo.spawnInfo.gunInfo[i].bulletConfig, gunSpawned, attackInfo.spawnInfo.gunInfo[i].shatterConfig));
+            StartCoroutine(spawnBullets(attackInfo.spawnInfo.gunInfo[i].bulletConfig, gunSpawned, attackInfo.spawnInfo.gunInfo[i].shatterConfig)); //Starts spawning bullets
 
-            yield return new WaitForSeconds(attackInfo.spawnInfo.spawnDelay);
+            yield return new WaitForSeconds(attackInfo.spawnInfo.spawnDelay); //The cooldown between spawning guns
         }
     }
-    IEnumerator rotateGun(gunSpawnInfo.spawnedGun attackInfo, GameObject gun)
+    IEnumerator rotateGun(gunSpawnInfo.spawnedGun attackInfo, GameObject gun) //Rotates the gun (duh)
     {
-        yield return new WaitForSeconds(attackInfo.rotationConfig.timeToStart);
+        yield return new WaitForSeconds(attackInfo.rotationConfig.timeToStart); //Waits for the start cooldown to finish until it starts rotating baptul bim
         Vector3 saveRotation = gun.transform.rotation.eulerAngles;
-        float rotationSteps = 500;
-        float degreesPerRotation = (attackInfo.rotationConfig.degrees / rotationSteps);
-        if(gun.GetComponent<SpriteRenderer>().flipY)
+        float rotationSteps = 500; //Amounts of times it updates the rotation before rotating back
+        float degreesPerRotation = (attackInfo.rotationConfig.degrees / rotationSteps); //#-of degrees per rotation step
+        if(gun.GetComponent<SpriteRenderer>().flipY) //If the Y is flipped, rotate the other way.
         {
             degreesPerRotation *= -1;
         }
-        while(gun != null)
+        while(gun != null) //Null check
         {
             for(int i = 0; i < rotationSteps; i++)
             {
-                if (gun != null)
+                if (gun != null) //Another null check (I'm paranoid)
                 {
                     gun.transform.Rotate(new Vector3(0, 0, degreesPerRotation));
                     yield return new WaitForSeconds(0.01f / attackInfo.rotationConfig.speedMultiplier);
                 }
             }
-            if(attackInfo.rotationConfig.rotatesBack)
+            if(attackInfo.rotationConfig.rotatesBack) //if it rotates back,start rotating back 
             {
                 degreesPerRotation *= -1;
             }
             else
             {
-                if(gun != null)
+                if(gun != null) //Null check
                 {
-                    Quaternion rotation = Quaternion.Euler(saveRotation);
+                    Quaternion rotation = Quaternion.Euler(saveRotation); //resets the rotation
                     gun.transform.rotation = rotation;
                 }
             }
         }
     }
-    IEnumerator followPlayer(gunSpawnInfo.spawnedGun attackInfo, GameObject gun)
+    IEnumerator followPlayer(gunSpawnInfo.spawnedGun attackInfo, GameObject gun) //Makes the gun rotation follow the player over time
     {
         while(gun != null)
         {
-            GameObject player = GameObject.FindGameObjectWithTag("Player");
-            Vector3 target = player.transform.position - gun.transform.position;
+            GameObject player = GameObject.FindGameObjectWithTag("Player"); //Finds the player object
+            Vector3 target = player.transform.position - gun.transform.position; //Makes a vector between the player and the gun
             gun.transform.rotation = Quaternion.LookRotation(Vector3.forward, target);
-            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, target);
+            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, target); //Sets the rotation towards the vector
             gun.transform.rotation = rotation * Quaternion.Euler(0, 0, 90);
-            yield return new WaitForSeconds(attackInfo.trackingAccuracy);
+            yield return new WaitForSeconds(attackInfo.trackingAccuracy); //The lower the accuracy, the slower it updates.
             
         }
     }
 
 
-    public IEnumerator spawnBullets(BulletSimple attackInfo, GameObject gunSource, shatterShotConfig shatterConfig = null)
+    public IEnumerator spawnBullets(BulletSimple attackInfo, GameObject gunSource, shatterShotConfig shatterConfig = null) //SPAWNS BULLETS
     {
-        yield return new WaitForSeconds(attackInfo.delayBeforeShooting);
-        for (int i = 0; i < attackInfo.bulletAmmount; i++)
+        yield return new WaitForSeconds(attackInfo.delayBeforeShooting); //Waits for a delay before it starts spawning bullets
+        for (int i = 0; i < attackInfo.bulletAmmount; i++) //Iterates for each bullet
         {
-            if(gunSource != null)
+            if(gunSource != null) //Another null check
             {
-                for(int j = 0; j < attackInfo.multiShootConfig.bulletMultiplier; j++)
+                for(int j = 0; j < attackInfo.multiShootConfig.bulletMultiplier; j++) //Iterates for each bullet stream
                 {
-                    float startAngle = (attackInfo.multiShootConfig.angle / 2) * -1;
-                    float angleChunks = attackInfo.multiShootConfig.angle / (attackInfo.multiShootConfig.bulletMultiplier - 1);
+                    float startAngle = (attackInfo.multiShootConfig.angle / 2) * -1; //Sets the angle to 50% of the total angle below where it starts
+                    float angleChunks = attackInfo.multiShootConfig.angle / (attackInfo.multiShootConfig.bulletMultiplier - 1); //Makes all bullet streams equally far away from eachother angle-wise
 
-                    GameObject shootBullet = Instantiate(attackInfo.bullet);
-                    if(startAngle != 0)
+                    GameObject shootBullet = Instantiate(attackInfo.bullet); //Creates the bullet
+                    if(startAngle != 0) //Sets the bullet offsets (used for the move bullet script)
                     {
                         shootBullet.GetComponent<moveBullet>().offset = (angleChunks * j) + startAngle;
                     }
@@ -187,9 +187,9 @@ public class bossAttack: MonoBehaviour
                     {
                         shootBullet.GetComponent<moveBullet>().offset = 0;
                     }
-                    if (shatterConfig != null)
+                    if (shatterConfig != null) //If there is a possibility of the bullet shattering, give the information for that.
                     {
-                        createBullet(shootBullet, attackInfo, gunSource, shatterConfig);
+                        createBullet(shootBullet, attackInfo, gunSource, shatterConfig); //Gives the bullet the information it needs
                     }
                     else
                     {
@@ -202,27 +202,27 @@ public class bossAttack: MonoBehaviour
                         {
                             kil = true;
                         }
-                        StartCoroutine(destroyBullet(shootBullet, attackInfo.beamConfig.lifespan, gunSource, kil));
+                        StartCoroutine(destroyBullet(shootBullet, attackInfo.beamConfig.lifespan, gunSource, kil)); //Instead of a bullet getting destroyed when they hit walls, beams get destroyed after a period of time
                     }
-                    if (attackInfo.dieEarly)
+                    if (attackInfo.dieEarly) //Lets it die early if it should (works exactly like beams except the guns do not get destroyed in the method)
                     {
                         StartCoroutine(destroyBullet(shootBullet, attackInfo.beamConfig.lifespan, gunSource));
                     }
                     Debug.Log(shootBullet.GetComponent<SpriteRenderer>().material.GetColor("_Color"));
-                    changeColour(attackInfo.colourConfig.stage, attackInfo.colourConfig.Colour, shootBullet);
-                    if (attackInfo.colourConfig.colourMode == colourChange.mode.line)
+                    changeColour(attackInfo.colourConfig.stage, attackInfo.colourConfig.Colour, shootBullet); //Sets the colour of the bullet
+                    if (attackInfo.colourConfig.colourMode == colourChange.mode.line) //Depending on the setting, the bullet color updates at different points and in different ways
                     {
                         attackInfo.colourConfig.stage = changeColourState(attackInfo.colourConfig.stage, attackInfo.colourConfig.Colour.Count);
                     }
                     if (attackInfo.colourConfig.colourMode == colourChange.mode.random)
                     {
-                        int newstageL = Random.Range(0, attackInfo.colourConfig.Colour.Count);
-                        attackInfo.colourConfig.stage = changeColourState(newstageL, attackInfo.colourConfig.Colour.Count);
+                        int newstageL = Random.Range(0, attackInfo.colourConfig.Colour.Count); //sets the stage (the colour in the array) randomly)
+                        attackInfo.colourConfig.stage = changeColourState(newstageL, attackInfo.colourConfig.Colour.Count); //Makes the bullet be another colour next time
                     }
                     test = GameObject.FindGameObjectsWithTag("bullet");
-                    Debug.Log(test.Length);
+                    Debug.Log(test.Length); //For debugging reasons, shows how many bullets are on screen (for stress testing)
                     float width = gunSource.GetComponent<SpriteRenderer>().bounds.size.x;
-                    if (gunSource.transform.Find("bulletSpawn") != null)
+                    if (gunSource.transform.Find("bulletSpawn") != null) //Puts the bullet in the correct part of its source. Depending on the "FlipY" propety of the source
                     {
                         if (gunSource.GetComponent<SpriteRenderer>().flipY)
                         {
@@ -236,10 +236,10 @@ public class bossAttack: MonoBehaviour
                     else
                     {
                         Debug.Log(placeBullet(gunSource));
-                        shootBullet.transform.position = placeBullet(gunSource);
+                        shootBullet.transform.position = placeBullet(gunSource); //Sets position correclty
                     }
-                    shootBullet.transform.rotation = gunSource.transform.rotation;
-                    if (startAngle != 0)
+                    shootBullet.transform.rotation = gunSource.transform.rotation; //The rotation of the bullet == the rotation of the source
+                    if (startAngle != 0) //Checks for 0 just to make sure the formula doesn't break
                     {
                         shootBullet.transform.Rotate(new Vector3(0, 0, shootBullet.transform.rotation.z + startAngle + (angleChunks * j)));
                     }
