9dbaa51312d54a01ba7398fda17d007a8a489663
Isak Nilsson <isak.nilsson7@learnet.se>
2022-09-15 13:40:16 +0200

    Added more documentation

--- Assets/bossAttack.cs
+++ Assets/bossAttack.cs
@@ -74,7 +74,7 @@ public class bossAttack: MonoBehaviour
             attackInfo.spawnInfo.gunInfo[i].bulletConfig.lockedToGun = attackInfo.spawnInfo.gunInfo[i].rotationConfig.lockedToGun;
             gunSpawned.transform.localScale = new Vector2(savedScale.x * attackInfo.spawnInfo.gunInfo[i].gunScaleX, savedScale.y * attackInfo.spawnInfo.gunInfo[i].gunScaleY);
             gunSpawned.transform.position = new Vector3(attackInfo.spawnInfo.gunInfo[i].spawnLocation.transform.position.x, attackInfo.spawnInfo.gunInfo[i].spawnLocation.transform.position.y, -3);
-            gunSpawned.GetComponent<SpriteRenderer>().sprite = attackInfo.spawnInfo.gunInfo[i].gunTexture;
+            gunSpawned.GetComponent<SpriteRenderer>().sprite = attackInfo.spawnInfo.gunInfo[i].gunTexture; //All of this just gives the information needed to create the guns and bullets, really long because it's high up in the information hierarchy.
             if(gunSpawned.GetComponent<SpriteRenderer>().sharedMaterial == attackInfo.spawnInfo.gunInfo[i].bulletConfig.bullet.GetComponent<SpriteRenderer>().sharedMaterial)
             {
                 changeColour(attackInfo.spawnInfo.gunInfo[i].bulletConfig.colourConfig.stage, attackInfo.spawnInfo.gunInfo[i].bulletConfig.colourConfig.Colour, gunSpawned); //Green screens the gun to the same colour as the bullet
@@ -243,18 +243,18 @@ public class bossAttack: MonoBehaviour
                     {
                         shootBullet.transform.Rotate(new Vector3(0, 0, shootBullet.transform.rotation.z + startAngle + (angleChunks * j)));
                     }
-                    shootBullet.GetComponent<moveBullet>().movementSpeedMultiplier = attackInfo.bulletSpeedMultiplier;
+                    shootBullet.GetComponent<moveBullet>().movementSpeedMultiplier = attackInfo.bulletSpeedMultiplier; //gives the bullet script a property from this script
                     if (shatterConfig != null)
                     {
                         if (shatterConfig.travelShot != null && shatterConfig.shootsWhileTraveling && gunSource != null)
                         {
-                            splitShot(shatterConfig.travelShot, shootBullet);
+                            splitShot(shatterConfig.travelShot, shootBullet); //Makes it shoot split bullets if you select it
                         }
                     }
                 }
 
-                yield return new WaitForSeconds(attackInfo.bulletDelay);
-                if(attackInfo.colourConfig.colourMode == colourChange.mode.row)
+                yield return new WaitForSeconds(attackInfo.bulletDelay); //Waits the delay between bullets
+                if(attackInfo.colourConfig.colourMode == colourChange.mode.row) //Another color setting
                 {
                     attackInfo.colourConfig.stage = changeColourState(attackInfo.colourConfig.stage, attackInfo.colourConfig.Colour.Count);
                 }
@@ -268,17 +268,17 @@ public class bossAttack: MonoBehaviour
         {
             if (!attackInfo.beam && gunSource.GetComponent<moveBullet>() == null)
             {
-                Destroy(gunSource);
+                Destroy(gunSource); //destroys the bullets after all bullets have been shot (provided the bullets are not beams and the source isn't a bullet)
             }
         }
     }
     public void splitShot(BulletSimple information, GameObject source)
     {
-        StartCoroutine(spawnBullets(information, source));
+        StartCoroutine(spawnBullets(information, source)); //spawns bullets but with less information as to not create an infinite loop
     }
     public GameObject createBullet(GameObject shootBullet, BulletSimple attackInfo, GameObject gunSource, shatterShotConfig shatterConfig = null)
     {
-        shootBullet.GetComponent<moveBullet>().bulletInfo = attackInfo;
+        shootBullet.GetComponent<moveBullet>().bulletInfo = attackInfo; //Gives the bullet scripts all information they require to function
         shootBullet.GetComponent<damagePlayer>().damage = attackInfo.bulletDamage;
         shootBullet.GetComponent<damagePlayer>().removeWhenHit = attackInfo.removeHit;
         shootBullet.GetComponent<damagePlayer>().removeWhenInvincible = attackInfo.removeInvincible;
@@ -286,7 +286,7 @@ public class bossAttack: MonoBehaviour
         shootBullet.GetComponent<moveBullet>().sourceScript = this;
         if(shatterConfig != null)
         {
-            if(shatterConfig.destoyShoot != null && shatterConfig.shootsWhenDestroyed)
+            if(shatterConfig.destoyShoot != null && shatterConfig.shootsWhenDestroyed) //Provides shatter information if present
             {
                 shootBullet.GetComponent<moveBullet>().shootsWhenDie = shatterConfig.shootsWhenDestroyed;
                 shootBullet.GetComponent<moveBullet>().dieProperties = shatterConfig.destoyShoot;
@@ -296,12 +296,12 @@ public class bossAttack: MonoBehaviour
     }
     public Vector3 placeBullet(GameObject location)
     {
-        return new Vector3(location.transform.position.x, location.transform.position.y, -3);
+        return new Vector3(location.transform.position.x, location.transform.position.y, -3); //function that copies the location except for the Z so that camera layering works properly
     }
-    public int changeColourState(int stage, int count)
+    public int changeColourState(int stage, int count) 
     {
-        stage++;
-        if(stage >= count)
+        stage++; //Increases the stage of the color (representing the n-th color in the list)
+        if(stage >= count) //resets the stage if the stage is greater or equal to the amount of items in the list of colors
         {
             stage = 0;
         }
@@ -309,16 +309,16 @@ public class bossAttack: MonoBehaviour
     }
     IEnumerator destroyBullet(GameObject shootBullet, float time, GameObject gunSource, bool kil = false)
     {
-        yield return new WaitForSeconds(time);
+        yield return new WaitForSeconds(time); //time before X is destroyed
         if(shootBullet != null && shootBullet.GetComponent<moveBullet>().shootsWhenDie && shootBullet.GetComponent<moveBullet>().dieProperties != null && this.gameObject.GetComponent<objectKiller>() != null)
         {
-            this.gameObject.GetComponent<objectKiller>().shatterDie(shootBullet, true);
+            this.gameObject.GetComponent<objectKiller>().shatterDie(shootBullet, true); //Makes the shot shatter on death if that option was picked
         }
         if(shootBullet != null)
         {
             Destroy(shootBullet);
         }
-        if(kil)
+        if(kil) //Destroys the source if all beams have been destroyed.
         {
             Destroy(gunSource);
         }
@@ -329,12 +329,12 @@ public class bossAttack: MonoBehaviour
         while(true)
         {
             int pickedAttack;
-            if (attackBasicList.Count > 1)
+            if (attackBasicList.Count > 1) //Needed so that the index isn't too low to function
             {
-                pickedAttack = Random.Range(0, attackBasicList.Count - 1);
+                pickedAttack = Random.Range(0, attackBasicList.Count - 1); //picks a random attack from the list of attacks
                 if (pickedAttack >= oldIndex)
                 {
-                    pickedAttack++;
+                    pickedAttack++; //Makes sure the same attack isn't picked twice.
                 }
             }
             else
@@ -343,7 +343,7 @@ public class bossAttack: MonoBehaviour
             }
             oldIndex = pickedAttack;
             IEnumerator attackCoroutine = null;
-            switch(attackBasicList[pickedAttack].Type)
+            switch(attackBasicList[pickedAttack].Type) //Runs the coroutine that corresponds to the correct attack
             {
                 case attackBasic.attackType.shootDown:
                     attackCoroutine = shootBullets(attackBasicList[pickedAttack]);
@@ -356,10 +356,10 @@ public class bossAttack: MonoBehaviour
                     
 
             }
-            if(attackCoroutine != null)
+            if(attackCoroutine != null) //null check
             {
                 StartCoroutine(attackCoroutine);
-                yield return new WaitForSeconds(attackBasicList[pickedAttack].duration);
+                yield return new WaitForSeconds(attackBasicList[pickedAttack].duration); //waits for the attacks duration
                 StopCoroutine(attackCoroutine);
             }
             else
@@ -368,7 +368,7 @@ public class bossAttack: MonoBehaviour
             }
         }
     }
-    void changeColour(int stage, List<Color> ColourList, GameObject whatToChange)
+    void changeColour(int stage, List<Color> ColourList, GameObject whatToChange) //Sets the color of the provided game object to the listed stage
     {
         whatToChange.GetComponent<SpriteRenderer>().material.SetColor("_Color", ColourList[stage]);
 
@@ -380,55 +380,55 @@ public class gunSpawnInfo
     [System.Serializable]
     public struct spawnedGun
     {
-        public GameObject spawnLocation;
-        public float rotation;
-        public GameObject gun;
-        public Sprite gunTexture;
-        public float gunScaleX;
+        public GameObject spawnLocation; //Where the gun spawns (takes the location's transform values)
+        public float rotation; //rotation of the gun (in Euler angles)
+        public GameObject gun; //The gun object prefab (used for bullet locations)
+        public Sprite gunTexture; //Incase I want to make reskins of the same prefab
+        public float gunScaleX; //scales for the weapon
         public float gunScaleY;
-        public bool shootTowardsPlayer;
-        public bool tracksPlayer;
+        public bool shootTowardsPlayer; //Sets the rotation to towards the player upon spawning
+        public bool tracksPlayer; //Sets the rotation towards the player every "trackingAccuracy" seconds
         public float trackingAccuracy;
-        public bool rotatesOverTime;
-        public BulletSimple bulletConfig;
-        public rotationSettings rotationConfig;
-        public shatterShotConfig shatterConfig;
+        public bool rotatesOverTime; //Turns on overtime rotations
+        public BulletSimple bulletConfig; //Contains all the bullet properties
+        public rotationSettings rotationConfig; //--=-- rotation properties
+        public shatterShotConfig shatterConfig; // --=-- shattering properties (both while traveling and on destruction
         int stage;
     }
-    public float spawnDelay;
-    public List<spawnedGun> gunInfo = new List<spawnedGun>();
+    public float spawnDelay; //Delay between each gun spawning
+    public List<spawnedGun> gunInfo = new List<spawnedGun>(); //List of all the guns in the attack
 }
 [System.Serializable]
-public class multiShootSettings
+public class multiShootSettings //Settings required for shooting multiple bullets in a cone shape
 {
     public int bulletMultiplier;
     public float angle;
 }
 [System.Serializable]
-public class colourChange
+public class colourChange //All settings for changing colors
 {
     public int stage;
     public enum mode
     {
-        line,
-        row,
-        random
+        line,  //Changing for every vertical line of bullets
+        row, //Changing for every horizontal line of bullets
+        random //Changes per-bullet randomly
     }
     public mode colourMode;
-    public List<Color> Colour = new List<Color>();
+    public List<Color> Colour = new List<Color>(); //Every color the bullets cycle through
 }
 [System.Serializable]
 public class beamSettings
 {
-    public float lifespan;
+    public float lifespan; //Also used for dying early (is the time in seconds it takes for a beam/bullet to get destroyed)
 }
 [System.Serializable]
 public class rotationSettings
 {
-    public float timeToStart;
-    public float degrees;
+    public float timeToStart; //Time before the gun starts rotating
+    public float degrees; //The amount of degrees it rotates before reseting/going back
     public float speedMultiplier;
-    public bool rotatesBack;
+    public bool rotatesBack;            
     public bool lockedToGun;
 }
 [System.Serializable]
@@ -445,12 +445,12 @@ public class BulletSimple
     public float growthMultiplier;
     public float scaleX; //bullet scale (is a multiplier)
     public float scaleY;
-    public bool removeInvincible;
-    public bool removeHit;
-    public bool incrementalGrowth;
-    public bool beam;
+    public bool removeInvincible; //Settings for if the bullet gets destroyed when you hit it while invincible
+    public bool removeHit; //same but you don't need to be invincible
+    public bool incrementalGrowth; //grows over time
+    public bool beam; //For all the differances between normal bullets and beams
     public bool dieEarly;
-    public multiShootSettings multiShootConfig;
+    public multiShootSettings multiShootConfig; //for all settings needed to shoot multiple bullets
     public colourChange colourConfig;
     public beamSettings beamConfig;
 }
@@ -459,7 +459,7 @@ public class BulletSimple
 public class shatterShotConfig
 {
     public bool shootsWhileTraveling;
-    public BulletSimple travelShot;
+    public BulletSimple travelShot; //contains the bullet class to give the information needed for what the bullet shot from the bullet should do
     public bool shootsWhenDestroyed;
     public BulletSimple destoyShoot;
 
