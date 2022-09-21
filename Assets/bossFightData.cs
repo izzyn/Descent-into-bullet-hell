f52c5da6aeca0050aff4f577b4fe8177f02e7427
Isak Nilsson <isak.nilsson7@learnet.se>
2022-09-21 22:45:34 +0200

    Fixed script not existing

---
+++ Assets/bossFightData.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossFightData : MonoBehaviour
+{
+    public List<bossdata> bossData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[System.Serializable]
+public class bossdata
+{
+    public GameObject boss;
+    public GameObject button;
+
+    [System.Serializable]
+    public enum selection
+    {
+        Heaven,
+        Hell,
+        Extra
+    }
+    public selection buttonPlace;
+}
